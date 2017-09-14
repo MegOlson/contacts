75d925b6fd27abfbce0967d22ac8e520de455863
Elrey Belmonti and Megan Olson <elrey@>
2017-09-14 14:16:56 -0700

    display contact details when name is clicked

--- app.rb
+++ app.rb
@@ -17,7 +17,11 @@ post ('/') do
   company = params["company"]
   new_contact = Contact.new(:type => type, :first_name => first_name, :last_name => last_name, :job_title => job_title, :company => company)
   new_contact.save
-  binding.pry
   @contacts = Contact.all
   erb(:input)
+end
+
+get('/contact/:id') do
+  @contact = Contact.find(params[:id])
+  erb(:contact)
 end
